350486bffb1671029c7285be626f66e0727b644d
Billy Lai <zhanlai12@gmail.com>
2023-05-27 20:49:34 -0500

    test page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-function load()
-{
-  fetch('./metadata.json')
-    .then(response => response.json())
-    .then(data => console.log(data))
+var moveData;
 
-  console.log("test");
+async function load()
+{
+  
+
+  fetch('./js/metadata.json')
+    .then(response => response.json())
+    .then(data => { moveData = data });
+    // .then(_ => generateMermaidGraph());
+
+    await generateMermaidGraph()
 }
 
-window.onload = function() {
-  console.log("on load");
+async function generateMermaidGraph()
+{
+  var list = ["test1", "test2", "test3"]
+  var random = Math.floor(Math.random() * 3); 
+
+  var graph = "graph TD\n";
+  graph += " " + list[random] + "-->C[fa:fa-ban forbidden]";
+
+  var graphElement = document.getElementById("graph");
+  graphElement.removeAttribute("data-processed");
+  document.getElementById("graph").innerHTML = graph;
+
+  mermaid.initialize();
+  await mermaid.run({
+    querySelector: '.test'
+  });
+
+  // 0000111-->C[fa:fa-ban forbidden]
+  // A[<img src='./images/000000010.png'>] --- B
+  // B-->D[<img src='./images/000000010.png'>];
+  // B-->E
 }
